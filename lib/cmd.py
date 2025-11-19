f9638e159aa22d58f10579bfb63da3a8a3b3ee67
Jeremy Davis <jeremy@turnkeylinux.org>
2025-11-19 04:06:57 +0000

    update sys.path to new pypy location

--- lib/cmd.py
+++ lib/cmd.py
@@ -25,9 +25,9 @@ import sys
 sys.path = [
     '',
     '/usr/lib/tklbam',
-    '/usr/lib/tklbam/deps/lib_pypy',
-    '/usr/lib/tklbam/deps/lib-python/2.7',
-    '/usr/lib/tklbam/deps/site-packages',
+    '/usr/lib/tklbam-pypy2/lib_pypy',
+    '/usr/lib/tklbam-pypy2/lib-python/2.7',
+    '/usr/lib/tklbam-pypy2/site-packages',
 ]
 from os.path import *
 from cliwrapper import CliWrapper
