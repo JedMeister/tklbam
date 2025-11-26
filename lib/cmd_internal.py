4f43ae141f4ada2d1dcf9243a2a96f4f97f6d2a8
Jeremy Davis <jeremy@turnkeylinux.org>
2025-11-26 00:33:22 +0000

    update python path called in cmd_internal.py

--- lib/cmd_internal.py
+++ lib/cmd_internal.py
@@ -27,7 +27,7 @@ main = CliWrapper.main
 
 def fmt_internal_command(command, *args):
     internal_command = [ realpath(__file__), command ] + list(args)
-    return fmt_command("python2", *internal_command)
+    return fmt_command("/usr/lib/tklbam-pypy2/bin/pypy", *internal_command)
 
 if __name__ == "__main__":
     CliWrapper.main()
