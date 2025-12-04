73e6abaf7486c40f1bab6b7c653112a4ad205795
Jeremy Davis <jeremy@turnkeylinux.org>
2025-12-04 22:29:17 +0000

    add deps/lib to sys.path

--- lib/cmd.py
+++ lib/cmd.py
@@ -25,6 +25,7 @@ import sys
 sys.path = [
     '',
     '/usr/lib/tklbam',
+    '/usr/lib/tklbam/deps/lib',
     '/usr/lib/tklbam-pypy2/lib_pypy',
     '/usr/lib/tklbam-pypy2/lib-python/2.7',
     '/usr/lib/tklbam-pypy2/site-packages',
