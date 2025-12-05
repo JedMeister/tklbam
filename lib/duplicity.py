33deddec9d2dc4a4d9a1c11944f5b44e1dea9f2f
Jeremy Davis <jeremy@turnkeylinux.org>
2025-12-05 01:01:04 +0000

    fix missing ')'

--- lib/duplicity.py
+++ lib/duplicity.py
@@ -59,7 +59,7 @@ class Duplicity:
             opts += [ ('archive-dir', '/var/cache/duplicity') ]
 
         opts = [ "--%s=%s" % (key, val) for key, val in opts ]
-        self.command = [join(PATH_DEPS_BIN, "duplicity"] + opts + list(args)
+        self.command = [join(PATH_DEPS_BIN, "duplicity")] + opts + list(args)
 
     def run(self, passphrase, creds=None, debug=False):
         sys.stdout.flush()
