e4409312c97a75aeef72e01cdac6962e76390b3d
Jeremy Davis <jeremy@turnkeylinux.org>
2026-06-03 14:07:50 +1000

    Minor updates...

--- update-dep-versions.sh
+++ update-dep-versions.sh
@@ -1,21 +1,32 @@
 #!/bin/bash -eu
 
+packages=(
+    python-pycurl
+    pycurl-wrapper
+    python-dateutil
+    python-six
+)
 
-tmp_file="$(mktemp -d)"
+tmp_dir="$(mktemp -d)"
 
 COMMIT_ID_PATH="$(pwd)/dep-commit-ids"
 
 rm -f "$COMMIT_ID_PATH"
 
-cd "$tmp_file"
-for pkg in python-crypto python-pycurl tklbam-duplicity turnkey-pylib tklbam-python-boto pycurl-wrapper; do
-    echo "# getting HEAD commit id for $pkg"
-    if [[ $pkg == "pycurl-wrapper" ]]; then
-        commit_id="$(git ls-remote "https://github.com/turnkeylinux/$pkg" | grep refs/heads/python2 | awk '{ print $1 }')"
-    else
-        commit_id="$(git ls-remote "https://github.com/turnkeylinux/$pkg" | grep HEAD | awk '{ print $1 }')"
+cd "$tmp_dir"
+for package in "${packages[@]}"; do
+    echo "# getting HEAD commit id for $package"
+    HEAD=HEAD
+    if [[ $package == "pycurl-wrapper" ]]; then
+        HEAD=refs/heads/python2
     fi
-    echo "$pkg:$commit_id" >> "$COMMIT_ID_PATH"
+    commit_id="$( \
+        git ls-remote "https://github.com/turnkeylinux/$package" \
+        | grep "$HEAD" \
+        | awk '{ print $1 }' \
+    )"
+    echo "$package:$commit_id" | tee -a "$COMMIT_ID_PATH"
+    echo
 done
 
-rm -r "$tmp_file"
+rm -r "$tmp_dir"
